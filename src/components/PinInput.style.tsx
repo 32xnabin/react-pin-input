8d09116291167cb06c0582ad4c2d7532675f2df6
nabin <nabin@dzangolab.com>
2021-09-05 07:21:49 +0545

    feat: add basic componet and style

---
+++ src/components/PinInput.style.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+interface ColorProps {
+  isCorrect?: boolean;
+}
+
+export const StyledPinInput = styled.input<ColorProps>`
+  width: 35px;
+  height: 35px;
+  border: 2px solid;
+  border-radius: 6px;
+  font-size: 12px;
+  text-align: center;
+  margin: 8px;
+  border-color: ${props => {
+    switch (props.isCorrect) {
+      case true:
+        return 'green'
+      case false:
+        return 'red'
+      default: 
+      return '#c3c3c3';
+    }
+  } }
+`;
+
+
---
+++ src/components/PinInput.tsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useRef } from 'react';
+import { StyledPinInput } from './PinInput.style';
+
+interface IProps {
+  pin: Array<number | undefined>;
+  onPinChanged: (pinEntry: number | undefined, index: number) => void;
+  pinLength: number;
+  validationMessage: string | undefined;
+  validationResult: boolean | undefined;
+  isValidating: boolean;
+}
+
+const PIN_MIN_VALUE = 0;
+const PIN_MAX_VALUE = 9;
+const BACKSPACE_KEY = 'Backspace';
+
+export const removeValuesFromArray = (valuesArray: string[], value: string) => {
+
+    const valueIndex = valuesArray.findIndex(entry => entry === value)
+    if(valueIndex === -1) {
+      return
+    }
+    valuesArray.splice(valueIndex, 1)
+  }
+
+const PinInput: React.FC<IProps> = ({
+  pinLength,
+  pin,
+  onPinChanged,
+  validationResult,
+  isValidating,
+}) => {
+  const inputRefs = useRef<HTMLInputElement[]>([]);
+
+  const changePinFocus = (pinIndex: number) => {
+    const ref = inputRefs.current[pinIndex];
+    if (ref) {
+      ref.focus();
+    }
+  };
+
+  useEffect(() => {
+    changePinFocus(0);
+  }, [isValidating]);
+
+  const onChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const previousValue = event.target.defaultValue;
+    const valuesArray = event.target.value.split('');
+    removeValuesFromArray(valuesArray, previousValue);
+    const value = valuesArray.pop();
+    if (!value) {
+      return;
+    }
+    const pinNumber = Number(value.trim());
+    if (isNaN(pinNumber) || value.length === 0) {
+      return;
+    }
+
+    if (pinNumber >= PIN_MIN_VALUE && pinNumber <= PIN_MAX_VALUE) {
+      onPinChanged(pinNumber, index);
+      if (index < pinLength - 1) {
+        changePinFocus(index + 1);
+      }
+    }
+  };
+
+  const onKeyDown = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const keyboardKeyCode = event.nativeEvent.code;
+    if (keyboardKeyCode !== BACKSPACE_KEY) {
+      return;
+    }
+
+    if (pin[index] === undefined) {
+      changePinFocus(index - 1);
+    } else {
+      onPinChanged(undefined, index);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        {Array.from({ length: pinLength }, (_, index) => (
+          <StyledPinInput
+            disabled={isValidating}
+            isCorrect={validationResult}
+            onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => onKeyDown(event, index)}
+            key={index}
+            ref={(el: HTMLInputElement) => {
+              if (el) {
+                inputRefs.current[index] = el;
+              }
+            }}
+            onChange={(event:any) => onChange(event, index)}
+            value={pin[index] || ''}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default PinInput;
